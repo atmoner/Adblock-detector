91a41c588086c9e8775067ed89e0e8745912afea
Atmo <contact.atmoner@gmail.com>
2017-02-25 22:00:37 +0100

    Update adblockDetector.js

--- adblockDetector.js
+++ adblockDetector.js
@@ -1,5 +1,11 @@
 var adBlockEnabled = false;
+var debug = true;
 var testAd = document.createElement('div');
+var returnMessage = document.getElementById('messageAdsblocker');
+var returnImage = document.getElementById('imageAdsblocker');
+var messageOk = document.getElementById('messageOk');
+var imageOk = document.getElementById('imageOk');
+
 testAd.innerHTML = '&nbsp;';
 testAd.className = 'adsbox';
 document.body.appendChild(testAd);
@@ -8,5 +14,13 @@ window.setTimeout(function() {
     adBlockEnabled = true;
   }
   testAd.remove();
-  console.log('AdBlock Enabled? ', adBlockEnabled)
+  if (debug) 
+  	console.log('AdBlock Enabled? ', adBlockEnabled)
+  if (adBlockEnabled) {
+  	returnMessage.style.display = 'block';
+  	returnImage.style.display = 'block';
+  } else {
+  	messageOk.style.display = 'block';
+  	imageOk.style.display = 'block';
+  }
 }, 100);
